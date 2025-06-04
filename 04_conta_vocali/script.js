6fc5368c90ff7a335de5dd11781e950436941c6c
FrancescoCellitti <francescocellitti@virgilio.it>
2025-06-04 16:49:59 +0200

    snacks 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,27 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const contaVocali = (str) => {
+    let count = 0;
+    let newStr = "";
+    for (i = 0; i < str.length; i++) {
+        if (['a', 'e', 'i', 'o', 'u'].includes(str[i])) {
+            count += 1;
+            if (newStr.length != 0) {
+                newStr = newStr + ", " + str[i]
+            } else {
+                newStr = newStr + str[i]
+            }
+
+        }
+
+    }
+    console.log(`${count} (${newStr})`)
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+contaVocali(word)
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
